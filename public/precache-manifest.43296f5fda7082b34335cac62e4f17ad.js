881b40c56c47b9d7153100abc19cae40a9405ee8
Deie <chantalsauvann@gmail.com>
2019-03-08 16:11:12 +0100

    css

---
+++ public/precache-manifest.43296f5fda7082b34335cac62e4f17ad.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "d3984b39b508a403d21bc0b34fb95e37",
+    "url": "/static/media/icon-2.d3984b39.svg"
+  },
+  {
+    "revision": "75f0c29383b06466f001",
+    "url": "/static/css/main.35aea31a.chunk.css"
+  },
+  {
+    "revision": "fdfcfda2d9b1bf31db52",
+    "url": "/static/js/runtime~main.fdfcfda2.js"
+  },
+  {
+    "revision": "442aee7e009c61db3168e22562886828",
+    "url": "/static/media/searchIcon.442aee7e.svg"
+  },
+  {
+    "revision": "c77114a9f7ca1823e8ed",
+    "url": "/static/js/2.c77114a9.chunk.js"
+  },
+  {
+    "revision": "e42f9e1248a21f1cf6a19c0424cc38d2",
+    "url": "/static/media/logo-little.e42f9e12.png"
+  },
+  {
+    "revision": "9ec5d5fe1415fb09fa0b5c05549afc06",
+    "url": "/static/media/icon-1.9ec5d5fe.svg"
+  },
+  {
+    "revision": "75f0c29383b06466f001",
+    "url": "/static/js/main.75f0c293.chunk.js"
+  },
+  {
+    "revision": "7da228b9ec2ebefd2df544fea5581418",
+    "url": "/static/media/icon-3.7da228b9.svg"
+  },
+  {
+    "revision": "1bdf7dd291498d6663b133c7dbea4933",
+    "url": "/static/media/icon-4.1bdf7dd2.svg"
+  },
+  {
+    "revision": "0fa01917df940573ef07aed9c8bd23d6",
+    "url": "/static/media/logo-big.0fa01917.png"
+  },
+  {
+    "revision": "ba9a92d6ac76eee12e814bdfc6e1fa3f",
+    "url": "/static/media/bgbg.ba9a92d6.svg"
+  },
+  {
+    "revision": "2bce2da830753a5642530bec5bd55981",
+    "url": "/static/media/bg-screen.2bce2da8.jpg"
+  },
+  {
+    "revision": "c77114a9f7ca1823e8ed",
+    "url": "/static/css/2.27728284.chunk.css"
+  },
+  {
+    "revision": "ac1ee570d33a6423b7e31669760ce9fc",
+    "url": "/index.html"
+  }
+];
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.801807a9cd942819cfc13b26333151ff.js"
+  "/precache-manifest.43296f5fda7082b34335cac62e4f17ad.js"
 );
 
 workbox.clientsClaim();
